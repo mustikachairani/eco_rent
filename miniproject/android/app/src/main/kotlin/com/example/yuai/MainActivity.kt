da0514da10a7a0bc70b2c267373823b96994004e
mustika chairani <mustikachrn5@gmail.com>
2023-11-03 18:06:50 +0700

    Delete

--- miniproject/android/app/src/main/kotlin/com/example/yuai/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yuai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- miniproject/lib/model/user_model.dart
+++
@@ -1,7 +0,0 @@
-// lib/models/user_model.dart
-class User {
-  final String email;
-  final String password;
-
-  User({required this.password, required this.email});
-}
