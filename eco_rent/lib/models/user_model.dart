7d66f6c5483205d835ed5bb32f3a3f869281f384
mustika chairani <mustikachrn5@gmail.com>
2023-11-03 17:51:14 +0700

    Update project

--- eco_rent/lib/models/user_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UserModel extends ChangeNotifier {
-  
-}
--- eco_rent/lib/view_models/catalog_view_model/equipment_list_view_model.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:flutter/material.dart';
-
-class EquipmentViewModel extends ChangeNotifier {
- 
-}
